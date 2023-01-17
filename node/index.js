4f0268beee460d399cdadf93f480433d372eb0f8
Karteris <karteris87@gmai.com>
2023-01-17 11:34:21 +0300

    законченая работа

--- node/index.js
+++ node/index.js
@@ -14,10 +14,6 @@ const PORT = process.env.PORT || 5000
 
 app.use(cors(corsOptions));
 
-// app.use(cors({
-// 	origin: 'http://185.99.154.132:3000', 
-// 	credentials: true
-// }))
 app.use(express.json())
 app.use(cookieParser())
 app.use('', router)
